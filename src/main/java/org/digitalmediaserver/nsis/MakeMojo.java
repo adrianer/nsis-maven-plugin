ed93c173c3fb16031d7fb1fe04e338efa3782ff6
Adrian Kalla <adrianer@users.noreply.github.com>
2025-02-22 00:57:17 +0100

    bustage fix

--- src/main/java/org/digitalmediaserver/nsis/MakeMojo.java
+++ src/main/java/org/digitalmediaserver/nsis/MakeMojo.java
@@ -31,6 +31,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
